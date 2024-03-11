e1c6f7af9ad346972ff6b4dbcad888e9829b63a4
sonsm0318 <160005229+sonsm0318@users.noreply.github.com>
2024-03-11 09:42:52 +0900

    Add files via upload

---
+++ Code1.ino
@@ -0,0 +1,12 @@
+void setup() {
+  // put your setup code here, to run once:
+  // 설정 함수
+  Serial.begin(9600); // 통신 속도 9600으로 설정
+  //Serial.println("test");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  // 반복 실행 함수
+  Serial.println("test");
+}
